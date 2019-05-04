f8e3f27385ff6c3a9e71fdcc5681e515c3cfbadc
hoangishs <38305536+hoangishs@users.noreply.github.com>
2019-05-05 01:10:29 +0700

    1

---
+++ arduino-test/arduino-test.ino
@@ -0,0 +1,70 @@
+#include <SoftwareSerial.h>
+
+#define debug true
+
+SoftwareSerial espSerial(3,2);
+
+uint8_t data_from_esp_buff[num_byte_from_esp];
+uint8_t data_from_esp_i=0;
+uint32_t last=0;
+uint32_t lastT=0;
+bool checkSum();
+
+void setup()
+{
+    Serial.begin(9600);
+    espSerial.begin(9600);
+    if(debug) Serial.println("setup done");
+}
+void loop()
+{
+    if(millis()-last>10000)
+    {
+        last=millis();
+        uint8_t data2esp_buff[23]={8,11,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,0,0};
+        uint16_t a=0;
+        for(uint8_t i=0;i<21;i++)
+        {
+            a+=data2esp_buff[i];
+        }
+        data2esp_buff[21]=a/256;
+        data2esp_buff[22]=a%256;
+        if(debug) Serial.print(" - data: ");
+        for(uint8_t i=0;i<22;i++)
+        {
+            espSerial.write(data2esp_buff[i]);
+            if(debug) Serial.print(data2esp_buff[i]);
+            if(debug) Serial.print(" ");
+        }
+        if(debug) Serial.println();
+    }
+    else if(millis()-lastT>3000)
+    {
+        lastT=millis();
+        espSerial.write(8);
+		espSerial.write(8);
+    }
+}
+
+bool checkSum()
+{
+    uint16_t check1=0;
+    for (uint8_t i = 0; i < num_byte_from_esp-2; ++i)
+    {
+        check1+=data_from_esp_buff[i];
+    }
+    uint16_t check2=data_from_esp_buff[num_byte_from_esp-2]*256+data_from_esp_buff[num_byte_from_esp-1];
+    if(check1==check2)
+    {
+        if(debug) Serial.print(millis());
+        if(debug) Serial.print(" - ");
+        if(debug) Serial.print(check1);
+        if(debug) Serial.print(":");
+        if(debug) Serial.println(check2);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
---
+++ esp01/StructDefine.h
@@ -0,0 +1,10 @@
+#ifndef StructDefine_h
+#define StructDefine_h
+
+#define DEBUG Serial1
+
+#define PACKET_SIZE 23
+
+#define PIN_BUTTON 0
+
+#endif
